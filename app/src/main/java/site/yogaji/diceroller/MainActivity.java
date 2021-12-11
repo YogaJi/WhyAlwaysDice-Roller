f6e6157a4447a0880ee63bd5d6c9a1b2b7d09ab6
YogaJi <y.disturbia@gmail.com>
2021-12-11 12:36:42 +0800

    add styling
    add theme styling
    add spinner custom
    adjust functions
    make comments

--- app/src/main/java/site/yogaji/diceroller/MainActivity.java
+++ app/src/main/java/site/yogaji/diceroller/MainActivity.java
@@ -1,5 +1,7 @@
 package site.yogaji.diceroller;
 
+//Yujia Ji A00246407
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.R.layout;
@@ -27,15 +29,14 @@ public class MainActivity extends AppCompatActivity
     private int currentDice;
     private String addDiceTextString;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
         //set preference
-//        PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
-//        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
+        // prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         //get button id
         Button rollOnceButton = findViewById(R.id.rollOnceButton);
@@ -44,8 +45,8 @@ public class MainActivity extends AppCompatActivity
         Button addRoll = findViewById(R.id.addRoll);
 
         //get rolling dices id
-//        TextView roll1Text = findViewById(R.id.roll1Text);
-//        TextView roll2Text = findViewById(R.id.roll2Text);
+        // TextView roll1Text = findViewById(R.id.roll1Text);
+        // TextView roll2Text = findViewById(R.id.roll2Text);
 
         //set listener
         rollOnceButton.setOnClickListener(this);
@@ -57,7 +58,7 @@ public class MainActivity extends AppCompatActivity
 
         //set spinner
         Spinner diceSpinner = findViewById(R.id.diceSpinner);
-        //add dice list for dice spinner
+        //add dice list to dice spinner
         List<String> diceList = new ArrayList<>();
         diceList.add("4");
         diceList.add("6");
@@ -67,13 +68,12 @@ public class MainActivity extends AppCompatActivity
         diceList.add("20");
         diceList.add("10s");
 
-        //add Adapter for diceSpinner
+        //add Adapter for diceSpinner & add spinner custom xml
         ArrayAdapter diceSpinnerAdapter = new ArrayAdapter<>(this, R.layout.custom_spinner_text_item, diceList);
         diceSpinnerAdapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
 
-
         diceSpinner.setAdapter(diceSpinnerAdapter);
-        //set spinner Listener
+        //set spinner listener
         diceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -96,14 +96,11 @@ public class MainActivity extends AppCompatActivity
         addDiceEdit.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
                 addDiceTextString = String.valueOf(addDiceEdit.getText());
-//                addDiceText = Integer.parseInt(addDiceTextString);
             }
 
             @Override
@@ -111,8 +108,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        //add dice listener
-
+        //add dice button listener
         addRoll.setOnClickListener(v -> {
             if(Objects.equals(addDiceTextString, "0")){
                 addDiceEdit.setText("");
@@ -120,17 +116,15 @@ public class MainActivity extends AppCompatActivity
                 //
             }else{
                 diceList.add(addDiceTextString);
+                Toast.makeText(MainActivity.this, "added!" , Toast.LENGTH_SHORT).show();
                 addDiceEdit.setText("");
-
             }
 
         });
 
-
     }//end of onCreate
 
-
-    @SuppressLint("NonConstantResourceId")
+    @SuppressLint({"NonConstantResourceId", "UseCompatLoadingForDrawables"})
     @Override
     public void onClick(View view) {
 
@@ -140,22 +134,27 @@ public class MainActivity extends AppCompatActivity
         int currentNum;
         switch (view.getId()) {
             case R.id.rollOnceButton:
-//                rollOnce
+                // rollOnce
                 if(currentDice == 0){
+                    //if dice is 10s
                     int rand10s = 10;
                     currentNum = (int) (Math.random()*rand10s+1)*10;
                 }else{
+                    //common dice
                     currentNum = (int) (Math.random()*currentDice+1);
                 }
                 roll1Text.setText(String.valueOf(currentNum));
-
-                roll2Text.setText("-");
+                //set dice 2 invisible
+                roll2Text.setText("");
+                roll2Text.setBackground(null);
                 break;
             case R.id.rollTwiceButton:
-//                rollTwice
+                //rollTwice
+                roll2Text.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_dice_frame));
+
                 if(currentDice == 0){
                     int rand10s = 10;
-
+                    //if dice is 10s
                     currentNum = (int) (Math.random()*rand10s+1)*10;
                     roll1Text.setText(String.valueOf(currentNum));
                     currentNum = (int) (Math.random()*rand10s+1)*10;
@@ -180,5 +179,4 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-
-}
+}//end of class
